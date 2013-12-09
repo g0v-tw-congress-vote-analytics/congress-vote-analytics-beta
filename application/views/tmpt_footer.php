1fea8a929be8de699397e777167c6f36071892be
fntsrlike <fntsrlike@gmail.com>
2013-12-09 14:39:53 +0800

    將原本的選單改成套用bootstrap導覽列

--- application/views/tmpt_footer.php
+++ application/views/tmpt_footer.php
@@ -1,3 +1,4 @@
+  </div> <!-- container-->
   <script type="text/javascript" src="<?php echo base_url("/assets/jquery-1.10.2.min.js");?>"></script>
   <script type="text/javascript" src="<?php echo base_url("/assets/bootstrap/js/bootstrap.min.js");?>"></script>
   </body>
--- application/views/tmpt_header.php
+++ application/views/tmpt_header.php
@@ -9,11 +9,38 @@
 </head>
 
 <body>
-  <h2>歡迎回來<?php echo $username;?></h2>
-  <a href="<?php echo base_url('member');?>">會員列表</a>
-  <a href="<?php echo base_url('member/update');?>">修改個資</a>
-  <a href="<?php echo base_url('issue');?>">議題表態</a>
-  <a href="<?php echo base_url('politician');?>">立委列表</a>
-  <a href="<?php echo base_url('portal/logout');?>">登出</a>
-<br/>
-<br/>
+
+
+<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="<?php echo base_url("/");?>">選票成份分析系統</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li><a href="<?php echo base_url('member');?>">會員列表</a></li>
+      <li><a href="<?php echo base_url('issue');?>">議題表態</a></li>
+      <li><a href="<?php echo base_url('politician');?>">立委列表</a></li>
+    </ul>
+    <form class="navbar-form navbar-left" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="輸入議題或立偉關鍵字">
+      </div>
+      <button type="submit" class="btn btn-default">搜尋</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="<?php echo base_url('member/update');?>">修改 <?php echo $this->session->userdata('username'); ?> 個資</a></li>
+      <li><a href="<?php echo base_url('portal/logout');?>">登出</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>  
+<div class="container">
+  
