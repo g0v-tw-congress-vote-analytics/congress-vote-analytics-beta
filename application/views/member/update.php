0d8f2a102b88556f1f0a9722284f081a64f1c25f
fntsrlike <fntsrlike@gmail.com>
2013-12-09 15:38:10 +0800

    將修改個資套上bootstrap

--- application/views/member/update.php
+++ application/views/member/update.php
@@ -1,9 +1,46 @@
-<form name="form" method="post" action="<?php echo $action; ?>">
-  <br/> 帳號：<input type="text" name="id" value="<?php echo $username;?>" readonly />(此項目無法修改) 
-  <br/> 密碼：<input type="password" name="pw" value="<?php echo $password;?>" /> 
-  <br/> 再一次輸入密碼：<input type="password" name="pw2" value="<?php echo $password;?>" />
-  <br/> 電話：<input type="text" name="telephone" value="<?php echo $telephone;?>" /> 
-  <br/> 地址：<input type="text" name="address" value="<?php echo $address;?>" /> 
-  <br/> 備註：<textarea name="other" cols="45" rows="5"><?php echo $other;?></textarea> 
-  <br/> <input type="submit" name="button" value="確定" />
-</form>    
+<h2 class="col-md-offset-3">修改個資</h2>
+<div class="row">
+  <form class="form-horizontal" role="form">
+    <div class="form-group">
+      <label for="input_" class="col-md-offset-1 col-md-2 control-label">帳號</label>
+      <div class="col-md-3">
+        <input type="text" class="form-control" name="id" id="input_id" value="<?php echo $username;?>" placeholder="帳號" readonly >
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="input_" class="col-md-offset-1 col-md-2 control-label">密碼</label>
+      <div class="col-md-3">
+        <input type="password" class="form-control" name="pw" id="input_" value="<?php echo $password;?>" placeholder="密碼">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="input_" class="col-md-offset-1 col-md-2 control-label">密碼確認</label>
+      <div class="col-md-3">
+        <input type="password" class="form-control" name="pw2" id="input_" value="<?php echo $password;?>" placeholder="密碼確認">
+      </div>
+    </div>  
+    <div class="form-group">
+      <label for="input_" class="col-md-offset-1 col-md-2 control-label">電話</label>
+      <div class="col-md-3">
+        <input type="text" class="form-control" name="telephone" id="input_" value="<?php echo $telephone;?>" placeholder="電話2">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="input_" class="col-md-offset-1 col-md-2 control-label">地址</label>
+      <div class="col-md-3">
+        <input type="text" class="form-control" name="address" id="input_" value="<?php echo $address;?>" placeholder="地址">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="input_" class="col-md-offset-1 col-md-2 control-label">備註</label>
+      <div class="col-md-3">
+        <textarea class="form-control" name="other" id="input_" placeholder="備註"><?php echo $other;?></textarea> 
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-md-offset-3 col-md-3">
+        <button type="submit" class="btn btn-default">確定</button>
+      </div>
+    </div>
+  </form>
+</div>
