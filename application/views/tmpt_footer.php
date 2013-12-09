4fc2853417ecc3f87768c272eb3f5b8168413c1d
fntsrlike <fntsrlike@gmail.com>
2013-12-09 14:30:16 +0800

    套入jquery, bootstrap

--- application/views/tmpt_footer.php
+++ application/views/tmpt_footer.php
@@ -1,2 +1,4 @@
+  <script type="text/javascript" src="<?php echo base_url("/assets/jquery-1.10.2.min.js");?>"></script>
+  <script type="text/javascript" src="<?php echo base_url("/assets/bootstrap/js/bootstrap.min.js");?>"></script>
   </body>
 </html>
--- application/views/tmpt_header.php
+++ application/views/tmpt_header.php
@@ -3,6 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <title><?php echo (isset($title)) ? "選票成份分析 - $title":'選票成份分析';?></title>
+  <link rel="stylesheet" href="<?php echo base_url('/assets/bootstrap/css/bootstrap.min.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('/assets/bootstrap/css/bootstrap-theme.min.css')?>">
+  
 </head>
 
 <body>
